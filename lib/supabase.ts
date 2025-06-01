1f8214f4ecca448e3a7875534cc8ca2d7d16abc5
Alber-Tadrous <albertadrous@gmail.com>
2025-06-01 15:07:43 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 import { EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_ANON_KEY } from '@env';
 
 export const supabase = createClient(
-  EXPO_PUBLIC_SUPABASE_URL,
-  EXPO_PUBLIC_SUPABASE_ANON_KEY,
+  'https://srdzhnxqbdntqmqyttfe.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyZHpobnhxYmRudHFtcXl0dGZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3ODQ4MDAsImV4cCI6MjA2NDM2MDgwMH0.ZXzGx_h_ybGX_eCDotvk8LtJx_b414wlhp1-srZb7a4',
   {
     auth: {
       persistSession: false,
