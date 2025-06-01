a04731a5c43f87d75d26ad0b3103f2a34d4aa1ee
Alber-Tadrous <106612395+Alber-Tadrous@users.noreply.github.com>
2025-06-01 14:45:46 -0400

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,8 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
-import { EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_ANON_KEY } from '@env';
+
+EXPO_PUBLIC_SUPABASE_URL=https://srdzhnxqbdntqmqyttfe.supabase.co
+EXPO_PUBLIC_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyZHpobnhxYmRudHFtcXl0dGZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3ODQ4MDAsImV4cCI6MjA2NDM2MDgwMH0.ZXzGx_h_ybGX_eCDotvk8LtJx_b414wlhp1-srZb7a4
 
 export const supabase = createClient(
   EXPO_PUBLIC_SUPABASE_URL,
